7d504e4fb57df9ffe6f42150b8462c0602a946d3
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-08-25 23:00:45 -0300

    ✨ added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import {theme } from './theme'
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
-import { Routes } from './routes';
 import 'react-native-gesture-handler';
+import { Home } from './pages/Home';
+import { StatusBar } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { Routes } from './routes';
 
 export const App: React.FC<{}> = () => {
   return (
-    <ThemeProvider theme={theme}>
     <NavigationContainer>
-    <StatusBar barStyle="default" backgroundColor="#cfc4e4" translucent />
-    <Routes />
-    </NavigationContainer>;
+    <ThemeProvider theme={theme}>
+       <StatusBar barStyle="default" backgroundColor="#5a11eb"  />
+     <Home />
     </ThemeProvider>
+    </NavigationContainer>
   );
    
 };
